975702709aa0c45e2299d452f4a56d4cc335a5fb
Fayez Al-kwiliy <alkwiliy.z@gmail.com>
2017-02-21 11:23:09 -0500

    This is hello world project

---
+++ HelloWorld/src/alKwiliy/HelloWorld.java
@@ -0,0 +1,16 @@
+package alKwiliy;
+/**
+ * HelloWorld
+ * Displays "Hello World"
+ * 21/02/17
+ * @author Fayez Al-kwiliy
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		
+
+	}
+
+}
